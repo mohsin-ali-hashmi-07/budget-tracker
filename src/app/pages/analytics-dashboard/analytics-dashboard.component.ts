33eefc6ce3b103ca4b971a886327130bf66f72ce
Mohsin Ali Hashmi <mohsin.hashmi@emumba.com>
2025-03-23 12:08:47 +0500

    added-package-canvas-analysis

--- src/app/pages/analytics-dashboard/analytics-dashboard.component.ts
+++ src/app/pages/analytics-dashboard/analytics-dashboard.component.ts
@@ -1,11 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { catchError, of, switchMap } from 'rxjs';
+import { UserService } from '../../services/user.service';
+import { UserDetailsService } from '../../services/user-details.service';
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-analytics-dashboard',
-  imports: [],
+  imports: [CanvasJSAngularChartsModule, MatCardModule, CommonModule],
   templateUrl: './analytics-dashboard.component.html',
   styleUrl: './analytics-dashboard.component.scss'
 })
-export class AnalyticsDashboardComponent {
+export class AnalyticsDashboardComponent implements OnInit {
+  private userService = inject(UserService);
+  private userDetails = inject(UserDetailsService);
 
+  isLoading = signal(true); 
+  expenses = signal<any[]>([]); 
+
+  lineChartOptions = () => {
+    const dataPoints = this.expenses().map((expense) => ({
+      label: new Date(expense.date).toLocaleDateString('en-US', { month: 'short' }),
+      y: expense.price, 
+    }));
+
+    return {
+      animationEnabled: true,
+     
+      theme: 'light2',
+      axisY: {
+        title: 'Value',
+      },
+      data: [
+        {
+          type: 'line',
+          dataPoints: dataPoints,
+        },
+      ],
+    };
+  };
+
+  ngOnInit(): void {
+    this.fetchExpenses();
+  }
+
+  fetchExpenses(): void {
+    const user = this.userDetails.getCurrentUser(); 
+
+    this.userService
+      .getUserById(user.id)
+      .pipe(
+        switchMap((userData: any) => {
+          const expenses = userData[0].expenses || [];
+          this.expenses.set(expenses); 
+          return of(expenses); 
+        }),
+        catchError((error) => {
+          return of([]); 
+        })
+      )
+      .subscribe(() => {
+        this.isLoading.set(false); // Data loaded
+      });
+  }
 }
